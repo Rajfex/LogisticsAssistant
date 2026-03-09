f3c05cab6700f2c6c54c343c36eb13f51d15b9b0
Jakub Wolodko <wolodko.jakub@gmail.com>
2026-03-09 11:52:31 +0100

    Add breaks to gantt chart

---
+++ LogisticsAssistant/Models/GantRow.cs
@@ -0,0 +1,10 @@
+﻿namespace LogisticsAssistant.Models
+{
+    public class GanttRow
+    {
+        public string TruckName { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+}
